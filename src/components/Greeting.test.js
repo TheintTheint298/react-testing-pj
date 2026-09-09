5814e013e83fe6ab1188e6c814844e8bf7a60784
TheintTheint298 <theinttheintsoehtet@gmail.com>
2026-09-09 16:38:05 +0630

    fix

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -9,6 +9,6 @@ test("renders Hello World as a text", () => {
   // ... nothing
 
   // Assert
-  const helloWorldElement = screen.getByText("Hello World", { exact: false });
+  const helloWorldElement = screen.getByText("Hello World!");
   expect(helloWorldElement).toBeInTheDocument();
 });
